06c2242c2644d22b6c8663c3ba6c939cedfa0c57
siyolisemekuto <siyozamekuto2.0@gmail.com>
2022-08-25 16:27:35 +0200

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <img :src="imgUrl" class="card-title"/>
+      <p class="card-text">Name</p>
+      <p class="card-text">Email</p>
+      <AboutView></AboutView>
+    </div>
+  </div>
   </nav>
   <router-view/>
 </template>
 
+<script>
+import AboutView from './views/AboutView.vue';
+export default {
+    name: "user icons",
+    data() {
+        return {
+            imgUrl: "https://i.postimg.cc/k4npt9HS/eyestetix-studio-l8q-IZm-Nu-D1-E-unsplash-1.jpg"
+        };
+    },
+    components: { AboutView }
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
